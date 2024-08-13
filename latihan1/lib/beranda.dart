73a61a58228722e9d27aef0bef05ae3aef84137f
putrafebriansyah7 <yan367381@gmail.com>
2024-08-13 12:02:14 +0700

    commit pertama

---
+++ latihan1/lib/beranda.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Beranda extends StatelessWidget {
+  const Beranda({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Hello world"),
+      ),
+    );
+  }
+}
